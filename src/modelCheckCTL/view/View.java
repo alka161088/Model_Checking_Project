cc2ecd988858e1362093d7820bc25db3010bd47b
modlanglais <morgandlanglais@gmail.com>
2020-06-28 17:38:19 -0500

    Deletes extra GUI files

--- src/modelCheckCTL/view/View.java
+++ src/modelCheckCTL/view/View.java
@@ -6,12 +6,10 @@ import modelCheckCTL.model.Model;
 public class View {
 	private Controller controller;
 	private Model model;
-//	private MainGui gui;
 	
 	public View(Controller c, Model m)
 	{
 		controller = c;
 		model = m;
-//		gui = new MainGui(m);
 	}
 }
