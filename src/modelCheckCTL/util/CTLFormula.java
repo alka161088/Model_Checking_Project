0953423b6253f3ae158fba98a674231f4b61bbee
alka161088 <alka161088@gmail.com>
2020-06-29 17:02:16 -0500

    right exp correction update

--- src/modelCheckCTL/util/CTLFormula.java
+++ src/modelCheckCTL/util/CTLFormula.java
@@ -478,7 +478,7 @@ public class CTLFormula {
             int openParanthesisCount = 0;
             int closeParanthesisCount = 0;
 
-            for (int i = 0; i < expression.length() - 1; i++) {
+            for (int i = 0; i < expression.length() ; i++) {
                 String currentChar = expression.substring(i, i + 1);
                 if (currentChar.equals(symbol) && openParanthesisCount == closeParanthesisCount) {
                     leftExpression = expression.substring(0, i);
@@ -521,12 +521,14 @@ public class CTLFormula {
             }
 
             if (openParanthesis - 1 == closeParanthesis)
-                newExpression = expression.substring(1, expression.length()-1);
-        } else if (!expression.startsWith("(") && expression.endsWith(")")) {
-            newExpression = expression.substring(0, expression.length() - 1);
-        } else if (expression.startsWith("(") && !expression.endsWith(")")) {
-            newExpression = expression.substring(1, expression.length()-1);
+                newExpression = expression.substring(1, expression.length()-2);
         }
+
+//        if (!expression.startsWith("(") && expression.endsWith(")")) {
+//            newExpression = expression.substring(0, expression.length() - 1);
+//        } else if (expression.startsWith("(") && !expression.endsWith(")")) {
+//            newExpression = expression.substring(1, expression.length() - 1);
+//        }
 
         System.out.println("new exp: " + newExpression);
 
